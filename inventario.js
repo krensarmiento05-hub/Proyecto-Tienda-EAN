790b302542f690a0dd6e6197c3456ff97a46b327
krensarmiento05-hub <krensarmiento05@gmail.com>
2026-07-01 12:01:18 -0500

    Update inventario.js

--- inventario.js
+++ inventario.js
@@ -1,5 +1,5 @@
 // Lee el archivo CSV y lo convierte en tabla
-fetch('INVENTARIO TIENDA 2026.csv')
+fetch('TIENDA 2026 INVENTARIO.csv')
   .then(r => r.text())
   .then(data => {
     const filas = data.split('\n').slice(1);
@@ -9,9 +9,9 @@ fetch('INVENTARIO TIENDA 2026.csv')
       if (columnas.length >= 14) {
         const material = columnas[0];
         const nombre = columnas[3];
-        const inventarioFisico = columnas[11];
-        const inventarioSAP = columnas[12];
-        const diferencia = columnas[13];
+        const inventarioFisico = columnas[3];
+        const inventarioSAP = columnas[4];
+        const diferencia = columnas[5];
         if (nombre) {
           tabla.innerHTML += `<tr><td>${material}</td><td>${nombre}</td><td>${inventarioFisico}</td><td>${inventarioSAP}</td><td>${diferencia}</td></tr>`;
         }
